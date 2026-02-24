1e49a3b6d6a08e496e1686132be2fe4a908252c8
dariocapucchio <dario.capucchio@gmail.com>
2026-02-23 23:54:38 -0300

    tratando de pedir la hora

--- flutter_app_04_multipage/lib/pages/loading.dart
+++ flutter_app_04_multipage/lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -9,6 +11,8 @@ class _LoadingState extends State<Loading> {
 
   void getData() async {
 
+    /*
+    
     print('Comenzo el request');
 
     // simulo datos que llegan de un servidor (delay)
@@ -22,12 +26,23 @@ class _LoadingState extends State<Loading> {
 
     print('$username - $bio');
 
+    */
+
+    Response response = await get(Uri.https('jsonplaceholder.typicode.com','/todos/1'));
+    Map data = jsonDecode(response.body);
+    print(data);
+  }
+
+  void getTime() async {
+    Response response = await get(Uri.https('time.now','/iana-time-zone/america-argentina-buenos-aires/'));
+    //Map timeData = jsonDecode(response.toString());
+    print(response.body);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
